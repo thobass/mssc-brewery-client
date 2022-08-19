d06df8daf62bcc7f73a918874c3b3dec90206795
BASSET, Thomas <thomas.basset@cgi.com>
2022-08-19 21:21:58 +0200

    RestTemplate implementation for beer.

---
+++ src/test/java/rocks/basset/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,43 @@
+package rocks.basset.msscbreweryclient.web.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import rocks.basset.msscbreweryclient.web.model.BeerDto;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = client.getBeerById(UUID.randomUUID());
+
+        assertNotNull(dto);
+    }
+
+    @Test
+    void saveNewBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        URI uri = client.saveNewBeer(beerDto);
+        assertNotNull(uri);
+    }
+
+    @Test
+    void updateBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        client.updateBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void deleteBeerById() {
+        client.deleteBeerById(UUID.randomUUID());
+    }
+}
